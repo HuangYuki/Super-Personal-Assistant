36a963d46e4ca85a9b699266c46f63aeff10b8c3
Huang-PC\Huang <f13579556@gmail.com>
2017-04-17 21:14:06 +0800

    Start!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Super Personal Assistant/Super Personal Assistant/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Super_Personal_Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.nameTabPage = new System.Windows.Forms.TabPage();
+            this.messageTabPage = new System.Windows.Forms.TabPage();
+            this.calendarTabPage = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.nameTabPage);
+            this.tabControl1.Controls.Add(this.messageTabPage);
+            this.tabControl1.Controls.Add(this.calendarTabPage);
+            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(448, 391);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // nameTabPage
+            // 
+            this.nameTabPage.Location = new System.Drawing.Point(4, 22);
+            this.nameTabPage.Name = "nameTabPage";
+            this.nameTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.nameTabPage.Size = new System.Drawing.Size(440, 365);
+            this.nameTabPage.TabIndex = 0;
+            this.nameTabPage.Text = "好友";
+            this.nameTabPage.UseVisualStyleBackColor = true;
+            // 
+            // messageTabPage
+            // 
+            this.messageTabPage.Location = new System.Drawing.Point(4, 22);
+            this.messageTabPage.Name = "messageTabPage";
+            this.messageTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.messageTabPage.Size = new System.Drawing.Size(440, 365);
+            this.messageTabPage.TabIndex = 1;
+            this.messageTabPage.Text = "訊息";
+            this.messageTabPage.UseVisualStyleBackColor = true;
+            // 
+            // calendarTabPage
+            // 
+            this.calendarTabPage.Location = new System.Drawing.Point(4, 22);
+            this.calendarTabPage.Name = "calendarTabPage";
+            this.calendarTabPage.Size = new System.Drawing.Size(440, 365);
+            this.calendarTabPage.TabIndex = 2;
+            this.calendarTabPage.Text = "行事曆";
+            this.calendarTabPage.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 391);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage nameTabPage;
+        private System.Windows.Forms.TabPage messageTabPage;
+        private System.Windows.Forms.TabPage calendarTabPage;
+    }
+}
+

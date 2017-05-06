85115517f916bd3d97d585e76617231852dec174
Meng-Ying LU <zxc22884@gmail.com>
2017-05-06 16:12:25 +0800

    新增Server(未有任何功能)
    書航和我在寫連線和Server部分

---
+++ Server/Server/ServerForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Server
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.ServerForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+

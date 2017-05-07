8e669cdff2d3c8ddc22768a993a6c4b041559ac4
Meng-Ying LU <zxc22884@gmail.com>
2017-05-07 16:00:23 +0800

    新增寫檔，新增比較功能(註冊部分)
    等等會完成連線測試

--- Server/Server/ManagementClass/ConnectionManagement.cs
+++ Server/Server/ManagementClass/ConnectionManagement.cs
@@ -1,6 +1,7 @@
 ﻿using Server.Class;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,42 @@ namespace Server.ManagementClass
     class ConnectionManagement
     {
         List<ClientAccount> _accountList = new List<ClientAccount>();
+        DataManagement _datamanagement = new DataManagement();
 
+
+        public ConnectionManagement()
+        {
+            InitializeAccount();
+        }
+
+        public void InitializeAccount() //搬到ConnectionManagement
+        {
+            string str = "0 aaaa bbbb fuck";
+            StreamReader sr = new StreamReader(@"../../DataStorage/Account.txt");
+            while (!sr.EndOfStream)
+            {
+                String[] words = null;
+                ClientAccount account = new ClientAccount();
+                string line = sr.ReadLine();
+
+                words = _datamanagement.seperateData(1, line);
+                account.Id = words[0];
+                account.Account = words[1];
+                account.Passward = words[2];
+                account.Name = words[3];
+                _accountList.Add(account);
+            }
+            sr.Close();
+            if(!_datamanagement.doDataManagement(str, _accountList))
+            {
+                Console.WriteLine("false");
+            }
+            //Console.WriteLine(_accountList.Count);
+            _datamanagement.doDataManagement(str, _accountList);
+            Console.WriteLine(_accountList.Count);
+        }
+
+        
 
     }
 }
--- Server/Server/ManagementClass/DataManagement.cs
+++ Server/Server/ManagementClass/DataManagement.cs
@@ -10,22 +10,27 @@ namespace Server.ManagementClass
 {
     class DataManagement
     {
-        ClientAccount _client = new ClientAccount();
-        List<String> _accountList = new List<String>();
+        ClientAccount clientAccount = new ClientAccount();
 
-        public bool doDataManagement(String data)
+        public bool doDataManagement(String data, List<ClientAccount> _accountList)
         {
             int dataTypeId = -1;
+            string[] words = null; //分離的字
 
             dataTypeId = getDataType(data);
-            seperateData(dataTypeId, data);
-            compareData(dataTypeId);
-
+            words = seperateData(dataTypeId, data);
+            if(dataTypeId == 0)
+            {
+                if (SaveClientAccountData(dataTypeId, words, _accountList) == false)
+                {
+                    return false;
+                }
+            }
 
             return true;
         }
 
-        public int getDataType(String data)
+        public int getDataType(String data) //資料型態
         {
             int typeId = 0;
 
@@ -34,33 +39,69 @@ namespace Server.ManagementClass
             return typeId;
         }
 
-        public String[] seperateData(int typeId, string data)
+        public String[] seperateData(int typeId, string data) //分離資料
         {
             char space = ' ';
-            string[] words = data.Split(space);
+            string[] words = null;
+
+            words = data.Split(space);
 
             return words;
-
         }
 
-        public bool compareData(int typeId)
+        public String mergeData(string[] words, int accountNumber) //重組資料
+        {
+            string newData = null;
+
+            for (int j = 0; j < 6 - accountNumber.ToString().Length; j++)
+            {
+                newData = newData + "0";
+            }
+            newData = newData + (accountNumber + 1).ToString();
+            clientAccount.Id = newData;
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                newData = newData + " " + words[i];
+            }
+
+            return newData;
+        }
+
+        public bool compareData(int typeId, String[] words, List<ClientAccount> _accountList) //比較資料
         {
             if(typeId == 0)
             {
-
+                for(int i = 0; i < _accountList.Count; i++)
+                {
+                    if(words[1] == _accountList[i].Account)
+                    {
+                        return false;
+                    }
+                }
             }
 
             return true;
         }
 
-        public bool saveData(String data)
+        public bool SaveClientAccountData(int typeId, string[] words, List<ClientAccount> _accountList) //存檔
         {
-            int typeId = data[0] - '0' ;
+            string newData = null;
+            StreamWriter sw = new StreamWriter(@"../../DataStorage/Account.txt", true);
 
-            if(typeId == 0)
+            if (compareData(typeId, words, _accountList) == false)
             {
-
+                sw.Close();
+                return false;
             }
+
+            newData = mergeData(words, _accountList.Count());
+            clientAccount.Account = words[1];
+            clientAccount.Passward = words[2];
+            clientAccount.Name = words[3];
+            _accountList.Add(clientAccount);
+            sw.WriteLine(newData);
+            sw.Close();
 
             return true;
         }

59781416d68495f2f35cd5af2f3976cdb5942a14
Wu Pin-Ru <angus013028@gmail.com>
2017-05-17 14:27:23 +0800

    行事曆編輯 & 刪除完成
    ※行事曆活動未紀錄提醒時間，需要修正
    ※程式裡 新增和編輯記帳 與 新增與編輯活動 中的Id為Text 用意不明

--- Super Personal Assistant/Super Personal Assistant/ManagementClass/AccountItemManagement.cs
+++ Super Personal Assistant/Super Personal Assistant/ManagementClass/AccountItemManagement.cs
@@ -23,9 +23,9 @@ namespace Super_Personal_Assistant
             _nextId++;
         }
 
-        public void deleteAccountItem()
+        public void deleteAccountItem(int id)
         {
-
+            _account.RemoveAt(id);
         }
 
         public bool changeAccountItem(int id, int cost, String name)
